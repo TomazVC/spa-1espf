ecece922a7a70bd1f9c6fad8bea32a68096b3fc7
tomazvcarballo <rm551417@fiap.com.br>
2023-09-04 09:56:34 -0300

    Props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Cabecalho from "./components/cabecalho";
 import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
+import viteLogo from "./assets/vite.svg";
 
 export default function App(){
+
+  let viteLogoAlt = "Vite Logo";
+
   return(
     <>
       <h1>APP - Componente principal!</h1>
@@ -14,7 +18,7 @@ export default function App(){
 
         {/* Iniciando a área de conteúdo */}
 
-        <Conteudo/>
+        <Conteudo viteLogoProps={viteLogo} viteLogoAltProps={viteLogoAlt}/>
 
         {/* Iniciando a área do rodapé */}
 
